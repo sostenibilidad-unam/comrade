b0eb5a445fcb2950088547a36dad54ee317e5f08
Fidel Serrano <serranoycandela@gmail.com>
2016-04-21 16:12:29 -0500

    the script to calibrate one tile

---
+++ calibra_uno.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo java -Xss100m -Dfile.encoding=UTF-8 -classpath :/Users/fidel/git/sostenibilidad-unam/crecimiento-urbano/jmatharray.jar:/Users/fidel/git/sostenibilidad-unam/crecimiento-urbano/jmathplot.jar: Calibradores.EvolucionanCuencaAgri /Users/fidel/400avos/x6y8
